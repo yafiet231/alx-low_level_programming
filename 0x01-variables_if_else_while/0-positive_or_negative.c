fef5f93b2ba17bd59debcb10fbd6da9727931bee
yafiet231 <abieleyasu893@gmail.com>
2023-10-06 13:58:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,35 @@
+#include <stdlib.h>
+
+#include <time.h>
+
 #include <stdio.h>
 
+
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints is number positive, zero or negative
+ * return: always (success)
+ *
+ */
+
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
