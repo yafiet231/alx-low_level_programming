204006b20e23f168af52c1cd8392f4c5953d4be9
yafiet231 <abieleyasu893@gmail.com>
2023-10-06 06:45:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * 
  * Return: Always 0 (Success)
  */
-int main(void
+int main(void)
 {
-        puts("\"programming is like building a multilingual puzzle\n");
+        puts("\"programming is like building a multilingual puzzle");
         return (0);
 }
