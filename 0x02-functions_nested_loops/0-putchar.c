a1985facf0f837e3374e6f808a520ee33dca7e17
yafiet231 <abieleyasu893@gmail.com>
2023-10-10 04:54:16 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * return: always 0
+ */
+
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('u');
+	_putchar('p');
+	-putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\m');
+	return (0);
+
+}
