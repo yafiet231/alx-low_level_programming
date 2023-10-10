df3c9e59375d94f70f5f838d33d75462cfdef9dc
yafiet231 <abieleyasu893@gmail.com>
2023-10-10 06:49:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  make alphabet
+ * return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'y'; c++)
+	{
+	putchar(c);
+	}
+	_putchar('\n');
+}
