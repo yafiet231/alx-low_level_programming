cfe8e5819eca0d77722f4f1ee89841c35ba324e9
yafiet231 <abieleyasu893@gmail.com>
2023-10-10 14:53:42 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet char
+ * @c: the char. to be checked
+ * return: 1 if c is a letter, lowercase or uppercase otherwise 0.
+ */
+int _isalpha(int c)
+	return (0);
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c >= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * -abs - computes the absolutevalue of an integer
+ * @n: the int to check
+ * return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
