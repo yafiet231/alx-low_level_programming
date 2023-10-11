f7d100d954065914e7448850e706eac7ad4c76b2
yafiet231 <abieleyasu893@gmail.com>
2023-10-11 12:59:13 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 
 
 /**
- * print_sign - computes the absolute of an integer
- * @n: the int to check
+ * print_sign -prints tthe sign of a number
+ * @n: the number to be checked
  *
- * return: 1 and prints + if n is greater than zero
- * and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * return: 1 for positive number, 0 for zero number or -1 for negative number
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar('+');
+	_putchar(43);
 	return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar (0);
-		return (0)
-		else
-		_putchar('-');
-		return (-1);
+		_putchar (45);
+		return (1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
