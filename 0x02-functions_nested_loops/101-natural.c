32b43028a3375f1685deb627d357e40dee9d491e
yafiet231 <abieleyasu893@gmail.com>
2023-10-11 14:29:42 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+/*
+*rile 101-natural.c
+*Auth: Abiel eyasu
+*/
+
+#include <stdio.h>
+
+/**
+ * man-lists all the natural numbers below 1024(excluded)
+ * that are multiples of 3 of 5
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0,
+	    for (i = 0; i <1024; i++)
+	    {
+		    if ((i % 3) == || (i % 5) == 0)
+			    sum += i;
+	    }
+	printf("%d\n", sum);
+}
+
+
