79cd9e76b430db92e20f67e6991c019d662381e1
yafiet231 <abieleyasu893@gmail.com>
2023-10-11 06:26:41 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: the character to be checked
  * Return: 1 for a letter, lowercase or uppercase or 0 otherwise
  */
-int_isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
