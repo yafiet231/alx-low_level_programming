451deef796040c644b0bec80d74721a0107161bd
yafiet231 <abieleyasu893@gmail.com>
2023-10-12 12:59:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers range 0 - 9
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 48; pr <= 57; pr++)
+	{
+		_putchar(pr);
+	}
+	_putchar('\n');
+}
