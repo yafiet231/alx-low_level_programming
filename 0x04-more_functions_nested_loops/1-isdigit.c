b6b07152dfaa2f6481da51383ed75109a73ff56b
yafiet231 <abieleyasu893@gmail.com>
2023-10-12 09:40:34 -0700

    find out if parameter is a number or an alphabet

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - check if parameter is a digit or character
+ *
+ * @c: parameter
+ * Return: 1 if its a number and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 43 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
