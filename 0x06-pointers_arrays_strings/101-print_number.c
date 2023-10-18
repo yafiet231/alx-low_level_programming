a90496fff2e165ca94e36182a7b9a192a8ef0e68
yafiet231 <abieleyasu893@gmail.com>
2023-10-18 16:28:25 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_number - Print an integer with putchar
+  * @n: Integer to be printed
+  *
+  * Return: Always (0)
+  */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
